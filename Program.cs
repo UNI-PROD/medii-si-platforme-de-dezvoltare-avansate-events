bd5a20fb6a1ccae5df747f255584289f19206847
Precup Eduard <precupeduard99@gmail.com>
2026-09-04 08:16:09 +0300

    setup things in program.cs, entry point of the app

--- Program.cs
+++ Program.cs
@@ -1,19 +1,64 @@
+using Microsoft.EntityFrameworkCore;
+using medii_si_platforme_de_dezvoltare_avansate_events.Data;
+using medii_si_platforme_de_dezvoltare_avansate_events.Repositories;
+using medii_si_platforme_de_dezvoltare_avansate_events.Services;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+
+// Configure SQLite
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+builder.Services.AddDbContext<SibiuEventsContext>(options =>
+    options.UseSqlite(connectionString)
+);
+
+// Register Repositories
+builder.Services.AddScoped<IUserRepository, UserRepository>();
+builder.Services.AddScoped<IEventRepository, EventRepository>();
+builder.Services.AddScoped<IEventRegistrationRepository, EventRegistrationRepository>();
+
+// Register Services
+builder.Services.AddScoped<IEventService, EventService>();
+builder.Services.AddScoped<IUserService, UserService>();
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(options =>
+{
+    options.SwaggerDoc("v1", new Microsoft.OpenApi.Models.OpenApiInfo
+    {
+        Title = "Sibiu Events API",
+        Version = "v1",
+        Description = "API pentru gestiunea evenimentelor din Sibiu",
+        Contact = new Microsoft.OpenApi.Models.OpenApiContact
+        {
+            Name = "Sibiu Events",
+            Url = new Uri("https://www.sibiuevents.com")
+        }
+    });
+});
 
 var app = builder.Build();
+
+// Apply migrations and seed data
+using (var scope = app.Services.CreateScope())
+{
+    var db = scope.ServiceProvider.GetRequiredService<SibiuEventsContext>();
+    db.Database.Migrate();
+}
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
-    app.UseSwaggerUI();
+    app.UseSwaggerUI(options =>
+    {
+        options.SwaggerEndpoint("/swagger/v1/swagger.json", "Sibiu Events API v1");
+        options.RoutePrefix = string.Empty; // Swagger UI at root
+    });
 }
 
 app.UseHttpsRedirection();
@@ -23,3 +68,4 @@ app.UseAuthorization();
 app.MapControllers();
 
 app.Run();
+
